5ea1b542c608be8bf90dda56efc253ba8bf18ff8
phattharawan24 <82808989+phattharawan24@users.noreply.github.com>
2021-05-27 14:47:23 +0700

    Add files via upload

---
+++ calculate_java.js
@@ -0,0 +1,24 @@
+function calculate(){
+    ttt();
+    setTimeout(function(){
+        var sum=0;
+        var kw= document.getElementById('kw').value;
+        var hr = document.getElementById('num1').value;
+        sum = kw*(hr*4);
+        document.getElementById('hour').value = sum.toFixed(2);
+        sum = sum / 0.7;
+        document.getElementById('drive').value = sum.toFixed(2);
+       
+     
+        var element1 = document.getElementById("divCar");
+        element1.classList.add("thing");
+        element1.classList.add("thing-1");
+    },200);
+  
+}
+
+function ttt(){
+    var element = document.getElementById("divCar");
+    element.classList.remove("thing");
+    element.classList.remove("thing-1");
+}
